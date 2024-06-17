31a0508999273100a1c405c23858fc7d99dc1eab
Ramis Khasianov <ramis.khasianov@gmail.com>
2024-06-17 11:32:01 +0500

    auth

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,12 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
-const page = () => {
-  return <div>Home</div>;
+const Home = () => {
+  return (
+    <div>
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
-export default page;
+export default Home;
